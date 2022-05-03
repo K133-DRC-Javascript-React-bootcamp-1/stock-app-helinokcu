0b33032b749846ac18b921ea4ce12cf9ce4ec091
Helin Okcu <helinnokcu@gmail.com>
2022-05-03 21:51:15 +0300

    project added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Medicine from './components/Medicine';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Medicine />
+    </div>
+  );
+}
+
+export default App;
